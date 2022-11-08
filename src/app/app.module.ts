284f5da3c8dcea3d279776e91f0e6ae44e9da6b8
cjporter25 <chris.j.porter25@gmail.com>
2022-11-07 18:46:26 -0600

    Additional scss changes, added camera plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Import the Cordova Camera Plugin - App Module TypeScript
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  //Remember to add the Camera plugin as a provider
+  providers: [Camera, 
+             { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+             ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,10 @@
+//--OVERWATCH 2 TANKS TAB-//
+//-----------TS-----------//
+
 import { Component, ViewChild } from '@angular/core';
 import { IonAccordionGroup } from '@ionic/angular';
+//Import the Cordova Camera Plugin - App Module TypeScript
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +13,30 @@ import { IonAccordionGroup } from '@ionic/angular';
 })
 export class Tab2Page {
 
-  constructor() {}
+  imgURL;
+
+  constructor(private camera: Camera) { }
+
+  getCamera(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.FILE_URI
+    }).then((res) => {
+      this.imgURL = res;
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+  getGallery(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((res) => {
+      this.imgURL = 'data:image/jpeg;base64,' + res;
+    }).catch(e => {
+      console.log(e);
+    })
+  }
 
 }
 
